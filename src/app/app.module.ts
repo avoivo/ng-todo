073c00b19c246415e9944dcd95e5a7a6b836e8bc
Athanasios Voivodas <tvoivodas@gmail.com>
2018-02-25 12:02:11 +0200

    adds suport for ngrx developer tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment"; // Angular CLI environemnt
 
 import { AppComponent } from "./app.component";
 import { TodosModule } from "./todos";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, TodosModule, StoreModule.forRoot({})],
+  imports: [
+    BrowserModule,
+    TodosModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production // Restrict extension to log-only mode
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
