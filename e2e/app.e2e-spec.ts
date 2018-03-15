8996a5c72066cf48148f0d90ae3e0243862de4f4
Athanasios Voivodas <tvoivodas@gmail.com>
2018-03-15 11:25:56 +0200

    adds some e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,51 @@ describe("ng-todo App", () => {
     page = new AppPage();
   });
 
+  async function expectTodoCountToBe(count: number) {
+    let todosCount = await page.getTodosCount();
+    expect(todosCount).toBe(count);
+  }
+
   it("should display welcome message", () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual("Todos");
+  });
+
+  it("should initialy contain zero todos", async () => {
+    await expectTodoCountToBe(0);
+  });
+
+  it("should add a todo as undone", async () => {
+    await page.addTodo("todo 1");
+    await expectTodoCountToBe(1);
+    let isDone = await page.isTodoDone(0);
+    expect(isDone).toBeFalsy();
+  });
+
+  it("should add a second todo and then mark it as done", async () => {
+    await page.addTodo("todo 2");
+    await expectTodoCountToBe(2);
+    await page.toggleTodo(1);
+    let isDone = await page.isTodoDone(1);
+    expect(isDone).toBeTruthy();
+  });
+
+  it("should filter the todo list by done todos", async () => {
+    await page.filterDone();
+    await expectTodoCountToBe(1);
+    expect(page.getTodoText(0)).toBe("todo 2");
+  });
+
+  it("should filter the todo list by undone todos", async () => {
+    await page.filterUndone();
+    await expectTodoCountToBe(1);
+    expect(page.getTodoText(0)).toBe("todo 1");
+  });
+
+  it("should filter the todo list by all todos", async () => {
+    await page.filterAll();
+    await expectTodoCountToBe(2);
+    expect(page.getTodoText(0)).toBe("todo 1");
+    expect(page.getTodoText(1)).toBe("todo 2");
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,4 +1,5 @@
-import { browser, by, element } from "protractor";
+import { browser, by, element, Key } from "protractor";
+import { promise } from "selenium-webdriver";
 
 export class AppPage {
   navigateTo() {
@@ -8,4 +9,48 @@ export class AppPage {
   getParagraphText() {
     return element(by.css("app-root h1")).getText();
   }
+
+  getTodosCount() {
+    return this.getTodos().count();
+  }
+
+  async addTodo(text: string) {
+    let input = element(by.css("input"));
+    await input.sendKeys(text);
+    await input.sendKeys(Key.ENTER);
+  }
+
+  toggleTodo(index: number) {
+    let todo = this.getTodos().get(index);
+    return todo.click();
+  }
+
+  async getTodoText(index: number): Promise<string> {
+    let todo = this.getTodos().get(index);
+    return todo.getText();
+  }
+
+  async isTodoDone(index: number): Promise<boolean> {
+    let todo = this.getTodos().get(index);
+    let classes = await todo.getAttribute("class");
+    return classes.split(" ").indexOf("done") !== -1;
+  }
+
+  filterAll = () =>
+    this.getFilters()
+      .get(0)
+      .click();
+
+  filterDone = () =>
+    this.getFilters()
+      .get(1)
+      .click();
+
+  filterUndone = () =>
+    this.getFilters()
+      .get(2)
+      .click();
+
+  private getTodos = () => element.all(by.css("todo-list > ul li"));
+  private getFilters = () => element.all(by.css("todo-filter-list > ul li"));
 }
